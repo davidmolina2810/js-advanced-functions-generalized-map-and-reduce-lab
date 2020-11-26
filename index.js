8a3984237977bd2cbb24e496b7d24e451a714f93
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-26 14:22:53 -0600

    commit tree 9a1d2f69bb8bf7dc681df195e0892847249e9897

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+const map = (arr, func) => {
+  let newArr = []
+  arr.forEach(el => {
+    newArr.push(func(el))
+  })
+  return newArr
+}
+
+const reduce = (arr, func, start) => {
+  let x = (!!start) ? start : arr[0]
+  let i = (!!start) ? 0 : 1
+  
+  for (; i < arr.length; i++) {
+    x = func(x, arr[i])
+  }
+
+  return x 
+}
